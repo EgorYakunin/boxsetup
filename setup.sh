59fdb92dfae202c22070f10767b93161e1c1e7f8
Egor Yakunin <e.yakunin04@gmail.com>
2024-05-26 22:44:20 +0300

    test script done

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 
 echo "Installing configs ..."
-cd ~/.config
-pwd
-ls -a
 
+# removing old config files
+rm -r ~/.config/nvim
+rm -r ~/.config/tmux
+rm ~/.bashrc
+rm ~/.bash_profile
 
+# copying new config files
+cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
+cp -r nvim ~/.config/
+cp -r tmux ~/.config/
+
+echo "Done :D"
