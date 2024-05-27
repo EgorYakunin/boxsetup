832e4becef6a3f9cc7240acab59324a720cd7224
Yakunin Egor <e.yakunin04@gmail.com>
2024-05-27 15:55:19 +0300

    script changed

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ rm ~/.bash_profile
 # copying new config files
 cp .bashrc ~/.bashrc
 cp .bash_profile ~/.bash_profile
-cp -r nvim ~/.config/
-cp -r tmux ~/.config/
+cp -r nvim ~/.config/nvim
+cp -r tmux ~/.config/tmux
 
 source ~/.bashrc
 
