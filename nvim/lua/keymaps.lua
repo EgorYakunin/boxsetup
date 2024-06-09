762dd161aa7fee29d8b82d8039820d562480b849
EgorYakunin <e.yakunin04@gmail.com>
2024-06-09 11:37:04 +0300

    some nvim changes

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,11 @@
 -- keymaps
 
+-- unmap space from default action of moving the cursor
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+
 vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('n', '<C-p>', ':e#<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-r>', ':!cargo run<CR>', {noremap = true, silent = true})
+
+-- save changes of the file and run it
+vim.api.nvim_set_keymap('n', '<C-r>', ':w | !cargo run<CR>', {noremap = true, silent = true})
