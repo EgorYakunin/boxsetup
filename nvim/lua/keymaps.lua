b8aaa778f9510730540efc5f35be4f566402bede
EgorYakunin <e.yakunin04@gmail.com>
2024-08-06 21:36:42 +0300

    updated nvim config

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,9 +6,15 @@ vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true 
 vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('n', '<C-p>', ':e#<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-b>', ':Telescope buffers<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-s>', ':%s/', {noremap = true, silent = true})
+
+-- remove highlight after search by pressing esc
+vim.api.nvim_set_keymap('n', '<Esc>', ':nohls<CR>', {noremap = true, silent = true})
 
 -- save changes of the file and run it
 
 -- rust
 -- vim.api.nvim_set_keymap('n', '<C-r>', ':w | !cargo run<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-r>', ':w | !yarn run script<CR>', {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n', '<C-r>', ':w | !yarn run script<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-r>', ':w | !gcc main.c; ./a.out<CR>', {noremap = true, silent = true})
